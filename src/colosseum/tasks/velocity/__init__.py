86d8c5463e52acbe574721f38730dbd62677c055
neverorfrog <97flavio.maiorana@gmail.com>
2026-06-14 12:38:00 +0200

    Update __init__.py

--- src/colosseum/tasks/velocity/__init__.py
+++ src/colosseum/tasks/velocity/__init__.py
@@ -1,5 +1,6 @@
 try:
   import colosseum.tasks.velocity.config.t1_23dof.t1_velocity_cfg  # noqa: F401
   import colosseum.tasks.velocity.config.t1_23dof_rma.t1_velocity_rma_cfg  # noqa: F401
+  import colosseum.tasks.velocity.config.t1_12dof.t1_velocity_cfg  # noqa: F401
 except ImportError:
   pass
