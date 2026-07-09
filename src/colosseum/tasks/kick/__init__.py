4ab9925217ecef5aa94c3e9d53eac3c4e7f37c4c
neverorfrog <97flavio.maiorana@gmail.com>
2026-07-09 20:09:34 +0900

    ops

--- src/colosseum/tasks/kick/__init__.py
+++ src/colosseum/tasks/kick/__init__.py
@@ -1,7 +1,4 @@
 try:
-  import colosseum.tasks.kick.config.t1.t1_dribbling_residual_cfg  # noqa: F401
-  import colosseum.tasks.kick.config.t1_kick.t1_dribbling_residual_kick_cfg  # noqa: F401
-  import colosseum.tasks.kick.config.t1_obstacles.t1_dribbling_residual_obstacles_cfg  # noqa: F401
-  import colosseum.tasks.kick.config.t1_stronger.t1_dribbling_residual_stronger_cfg  # noqa: F401
+  import colosseum.tasks.kick.config.t1.t1_kick_cfg  # noqa: F401
 except ImportError:
   pass
